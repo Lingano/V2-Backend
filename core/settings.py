0f91233d41033cc78d035ed1d8b01f647186984c
CuPidev <mateusz.cupryniak@usi.ch>
2025-05-13 20:06:57 +0200

    Update ALLOWED_HOSTS to include production and beta API domains

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-0$$@$)6e&6$w==d(in8^u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com', 'api.lingano.live', 'lingano.live', 'beta.lingano.live']
 
 
 # Application definition
